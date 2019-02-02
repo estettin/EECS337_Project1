db7eb8a562abd355e1e64ec4b86b6ffc1dd5442a
jakereifer <reifer.jake@gmail.com>
2019-02-02 11:28:09 -0600

    Changed sentiment into function (kev/jake)

--- sentiment.py
+++ sentiment.py
@@ -17,42 +17,51 @@ stringlist = []
 stops = set(stopwords.words('english'))
 stops.update(["host","hosts","hosting","goldenglobes", "golden", "globes", "rt", "http"])
 
-for i in range(0,len(data)):
-	if "Tina Fay" in data[i] or "Amy Poehler" in data[i] or "tina fay" in data[i] or "amy poehler" in data[i]:
-		string = ''.join([i if ord(i) < 128 else '' for i in data[i]])
-		j = 0
-		tstring = word_tokenize(string)
-		slen = len(tstring)
-		while j < slen:
-			if not tstring[j].isalpha():
-				tstring.pop(j)
-				slen = slen - 1
-				continue
-			if tstring[j].lower() == "rt" or tstring[j].lower() == "http" or tstring[j].lower() == "goldenglobes":
-				tstring.pop(j)
-				slen = slen - 1
-				continue
-			j = j + 1
-		stringlist.append(' '.join(tstring))
+def sentinmentOfPeople(people, data):
+	# capital or non capital 
+	for i in range(0,len(data)):
+		found = False
+		if people[0] in data[i] or people[0].lower() in data[i]:
+			string = ''.join([i if ord(i) < 128 else '' for i in data[i]])
+			found = True
+		elif len(people) > 1 and (people[1] in data[i] or people[1].lower() in data[i]):
+			string = ''.join([i if ord(i) < 128 else '' for i in data[i]])
+			found = True
+		if found:
+			j = 0
+			tstring = word_tokenize(string)
+			slen = len(tstring)
+			while j < slen:
+				if not tstring[j].isalpha():
+					tstring.pop(j)
+					slen = slen - 1
+					continue
+				if tstring[j].lower() == "rt" or tstring[j].lower() == "http" or tstring[j].lower() == "goldenglobes":
+					tstring.pop(j)
+					slen = slen - 1
+					continue
+				j = j + 1
+			stringlist.append(' '.join(tstring))
 
- 
+	sid = SentimentIntensityAnalyzer()
+	sum = 0
+	for sentence in stringlist:
+		ss = sid.polarity_scores(sentence)
+		sum = ss["compound"] + sum
+		#for k in sorted(ss):
+			#print('{0}: {1}, '.format(k, ss[k]), end='')
+	avgscore = sum/len(stringlist)
+	#print("Avg score = " + str(avgscore))
+	if avgscore > .5 :
+		print("Very Positive")
+	elif avgscore > .25:
+		print("Positive")
+	elif avgscore < -.5:
+		print("Very Negative")
+	elif avgscore < -.25:
+		print("Negative")
+	else:
+		print("Neutral")
 
-sid = SentimentIntensityAnalyzer()
-sum = 0
-for sentence in stringlist:
-	ss = sid.polarity_scores(sentence)
-	sum = ss["compound"] + sum
-	#for k in sorted(ss):
-		#print('{0}: {1}, '.format(k, ss[k]), end='')
-avgscore = sum/len(stringlist)
-print("Avg score = " + str(avgscore))
-if avgscore > .5 :
-	print("Very Positive")
-elif avgscore > .25:
-	print("Positive")
-elif avgscore < -.5:
-	print("Very Negative")
-elif avgscore < -.25:
-	print("Negative")
-else:
-	print("Neutral")
+sentinmentOfPeople(["Tina Fey", "Amy Poehler"], data)
+
