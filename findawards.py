7f071896bc0893f7ab5921545ef87cc20efdb2d6
ke-chan <32466454+ke-chan@users.noreply.github.com>
2019-02-04 16:55:46 -0600

    find awards file

--- findawards.py
+++ findawards.py
@@ -8,9 +8,26 @@ from tweet_parser import df_2013
 import spacy
 
 tweets = df_2013
+punc = [".",":","-","!","?"]
+"""
+Phrases to look for:
+wins _______ for
+the award for
+wins
+"""
 
 def FindAwards(data):
 	"""
 	takes in the tweets as an an array of string and identies award names
 	"""
-	
+	phrases = {}
+	nlp = spacy.load("en_core_web_sm")
+	for tweet in tweets:
+		if "best" or "Best" in tweet:
+			
+
+
+
+
+
+
