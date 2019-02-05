18c496b8d4535bc6a3ba4855668c1548ee69bf69
ke-chan <32466454+ke-chan@users.noreply.github.com>
2019-02-05 14:43:16 -0600

    basic find awards function

--- findawards.py
+++ findawards.py
@@ -4,13 +4,14 @@ from pprint import pprint
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
 import re
-from tweet_parser import df_2013
+from populate_db import tweets2013
 import spacy
 
-tweets = df_2013
-punc = [".",":","-","!","?"]
+tweets = tweets2013
+punc = [".",":","-","!","?","#"]
 lhs = ["wins", "Wins", "for", "wins" "#goldenglobe" "golden globe" "Golden Globe"]
 rhs = ["goes", ""]
+
 """
 Phrases to look for:
 wins _______ for
@@ -20,12 +21,37 @@ wins
 
 def FindAwards(data):
 	"""
-	takes in the tweets as an an array of string and identies award names
+	takes in the tweets as an an array of string and identifies award names
 	"""
 	phrases = {}
 	nlp = spacy.load("en_core_web_sm")
-	for tweet in tweets:
-		if "best" or "Best" in tweet:
+	for tweet in data:
+		if " best " in tweet.lower():
+			s = ""
+			doc = nlp(tweet)
+			flag = False
+			for element in doc:
+				if element.text.lower() == "best":
+					flag = True
+				if (element.text in punc or element.pos_ == "VERB" or element.pos_ == "ADP") and flag:
+					break
+				if flag:
+					s = s + element.text.lower() + " "
+			if s != "best":
+				if s in phrases:
+					phrases[s] += 1
+				else:
+					phrases[s] = 1
+	k = list(phrases.keys()).copy()
+	for key in k:
+		if phrases[key] <= 1:
+			del phrases[key]
+	return phrases
+
+	
+x = FindAwards(tweets[0:50000])
+
+
 			
 
 
