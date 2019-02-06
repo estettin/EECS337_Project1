a9b39d489c63a0fff45a823593d94d32c2fb2ecf
ke-chan <32466454+ke-chan@users.noreply.github.com>
2019-02-06 12:06:40 -0600

    update findawards

--- findawards.py
+++ findawards.py
@@ -8,7 +8,7 @@ from populate_db import tweets2013
 import spacy
 
 tweets = tweets2013
-punc = [".",":","-","!","?","#"]
+punc = [".",":","!","?","#"]
 lhs = ["wins", "Wins", "for", "wins" "#goldenglobe" "golden globe" "Golden Globe"]
 rhs = ["goes", ""]
 
@@ -26,6 +26,8 @@ def FindAwards(data):
 	phrases = {}
 	nlp = spacy.load("en_core_web_sm")
 	for tweet in data:
+		if tweet[0:2] == "RT":
+			continue
 		if " best " in tweet.lower():
 			s = ""
 			doc = nlp(tweet)
@@ -36,9 +38,9 @@ def FindAwards(data):
 				if (element.text in punc or element.pos_ == "VERB" or element.pos_ == "ADP") and flag:
 					#if the word is a punction, verb, or adposition ("for"), stop
 					break
-				if flag:
+				if flag and element.text != ",":
 					#if we have seen the word best, add the word to the string
-					s = s + element.text.lower() + " "
+					s = s + element.text.lower()
 			if s != "best":
 					#add to the dictionary
 				if s in phrases:
@@ -49,12 +51,13 @@ def FindAwards(data):
 	#remove keys that have only one occurance - these are most likely insignificant
 	k = list(phrases.keys()).copy()
 	for key in k:
-		if phrases[key] <= 1:
+		if phrases[key] <= 3:
 			del phrases[key]
 	return phrases
 
 	
 x = FindAwards(tweets[0:50000])
+
 
 
 			
